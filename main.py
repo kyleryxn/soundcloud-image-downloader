1d6831ab7d5567d2dc62c5b2f7ea707fba9c6fb8
Kyle <kyle.schoenhardt@mail.citytech.cuny.edu>
2022-11-29 02:19:55 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import os
+from sid import downloader
 
 if __name__ == '__main__':
+    directory = 'C:/Users/Kyle/Music/Electronic/'
 
+    url = input(f'Enter the url: ')
+    destination = input(f'\nDirectory to save the image: ')
+
+    complete_name = os.path.join(directory, destination)
+
+    downloader.image_downloader(url, complete_name)
 
 
